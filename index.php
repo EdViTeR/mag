9dbb86396c5a1eecd1b7d96e6c302895a44aefb9
EdViTeR <maxpivnnko@mail.ru>
2025-06-18 03:20:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<header class="header">
+		<div class="container">
+			<div class="header__inner">
+				<div class="header__logo">
+				</div>
+				
+				<nav class="nav">
+					<a class="nav__link" href="#">О нас</a>
+					<a class="nav__link" href="#">Направления подготовки</a>
+					<a class="nav__link" href="#">Контакты</a>
+				</nav>				
+			</div>
+		</div>
+	</header>
+
+	<div class="intro">
+		<div class="container">
+			<h1 class="intro__title">Откройте двери в мир финансов и бизнес-аналитики с магистерской программой <br><span class="blue__text">«Финансовая экономика и бизнес-аналитика»!</span></h1>
+
+			<div class="intro__group">
+				<div class="button__group">
+					<a class="order__button" href="#">Подать заявку</a>
+					<a class="consult__buttons" href="#">Получить онлайн-консультацию</a>
+				</div>
+				<div class="group__text">
+					<h3>Входит в <span class="blue__text">Топ-20</span> лучших рейтинга факультетов московских вузов по версии hh.ru</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<section class="section__text">
+		<div class="container">
+			<p>Вы мечтаете о карьере в финансовом секторе? Хотите стать экспертом в области бизнес-аналитики и получить уникальные знания, которые помогут вам выделиться на рынке труда? <span class="text__section__white">Тогда наша магистерская программа <span class="text__section__bold">"Финансовая экономика и бизнес-аналитика"</span> — это именно то, что вам нужно!</span></p>
+		</div>
+	</section>
+
+	<section class="why__section">
+		<div class="container">
+			<div class="top__why">
+				<div class="left__why">Почему стоит выбрать нашу программу?</div>
+				<div class="right__why">
+					<div class="right__head__block">
+						<p class="card__head__text">Команда профессионалов</p>
+						<p class="card__info__text">Наши преподаватели — это опытные практики и ученые, которые готовы делиться своими знаниями и опытом, помогая вам развиваться и достигать успеха.</p>
+					</div>
+					<div class="right__bottom__block">
+						<p class="card__head__text">Стажировка и трудоустройство</p>
+						<p class="card__info__text">Это ваш шанс начать карьеру в финансовом секторе с реальным опытом работы и ценными контактами в ведущих финансовых организациях России и организациях реального сектора экономики.</p>
+					</div>
+				</div>
+			</div>
+			<div class="bottom__why">
+				<div class="bottom__block">
+					<p class="card__head__text">Современные знания и навыки</p>
+					<p class="card__info__text">Программа включает в себя дисциплины по финансовой аналитике, риск-менеджменту, по использованию информационных технологий в задачах обработки финансово-экономической информации.</p>
+				</div>
+				<div class="bottom__block__end">
+					<p class="card__head__text">Бизнес-партнёрства</p>
+					<p class="card__info__text">Наш партнёр один из крупнейших банков России — банк ВТБ, который активно участвует в образовательном процессе, а это значит, что вы получите актуальные знания и практический опыт.</p>
+				</div>
+		</div>
+	</section>
+	<!-- <img src="assets/img/wave.svg" class="background-image" alt="Фоновое изображение"> -->
+	<section class="section__programs">
+		<div class="container">
+			<div class="top__programs">
+				<div class="left__top__programs">
+					<p>Магистратура РГУ <br>им. А.Н. Косыгина — это:</p>
+				</div>
+				<div class="right__top__programs">
+					<div class="top__big__programs">
+						<div class="price">от 120 000+₽</div>
+						<div class="price__description">По данным hh.ru средняя зарплата от 120 000 РУБ. без опыта работы</div>
+					</div>
+					<div class="right__little__programm">
+						<div class="top__little__programs">Возможность обучаться бесплатно</div>
+						<div class="top__little__programs__end">Вечерний график обучения и возможность совмещения с работой</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="middle__programs">
+				<div class="first__middle__programs">
+					<div class="little__programm">Отсрочка от призыва на военную службу</div>
+					<div class="little__programm">Получение совершенно новой профессии</div>
+					<div class="little__programm">Поступление по внутренним экзаменам</div>
+					<div class="little__programm__end">Продолжение обучения в аспирантуре</div>
+				</div>
+			</div>
+
+			<div class="middle__programs">
+				<div class="little__programm">Индустриальные партнеры,стажировки и практики в ведущих компаниях</div>
+				<div class="little__programm">Модульная система обучения</div>
+				<div class="little__programm">Возможность реализовать свою научную идею во время обучения</div>
+				<div class="little__programm__end">Возможность получить грантовую поддержку на свой стартап</div>
+			</div>
+
+			<div class="last__programs">
+				<div class="last__programm__card">Программа академической мобильности с зарубежными партнёрами</div>
+				<div class="last__programm__card">Сетевые международные программы с возможностью получения двух дипломов.</div>
+				<div class="last__programm__end">Образовательный кластер в центре Москвы, 5 минут от м. Шаболовская, 10 минут от м.Октябрьская</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="numerals">
+		<div class="container">
+			<div class="top__programs">
+				<div class="left__top__programs">
+					<p>Университет Косыгина<br> в цифрах</p>
+				</div>
+				<div class="right__top__programs">
+					<div class="right__little__programm numerals__card">
+						<div class="top__little__programs numerals__card__info">
+							<div class="price">100+</div>
+							<div class="price__description__numerals"> лет в образовании</div>
+						</div>
+						<div class="top__little__programs__end numerals__card__info">
+							<div class="price">100+</div>
+							<div class="price__description__numerals"> опытных преподавателей-практиков</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="middle__programs">
+				<div class="little__programm__numerals">
+					<div class="price">200+</div>
+					<div class="price__description__numerals">образовательных программ</div>
+				</div>
+				<div class="little__programm__numerals">
+					<div class="price">400+</div>
+					<div class="price__description__numerals"> компаний- партнеров</div>
+				</div>
+				<div class="little__programm__numerals">
+					<div class="price"> 13000+</div>
+					<div class="price__description__numerals">  обучающихся</div>
+				</div>
+				<div class="little__programm__numerals__end">
+					<div class="price">3000+</div>
+					<div class="price__description__numerals"> мест в общежитии</div>
+				</div>
+			</div>
+
+			<div class="middle__programs__bottom">
+				<div class="little__programm__numerals">
+					<div class="price">+2000</div>
+					<div class="price__description__numerals">внебюджетных мест</div>
+				</div>
+				<div class="little__programm__numerals">
+					<div class="price">5</div>
+					<div class="price__description__numerals">иностранных языков на выбор</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <section class="faq-section">
+    	<div class="container">
+	    	<h2>Вопросы и ответы:</h2>
+			<div class="faq-item">
+				<button class="faq-question">Что вы будете изучать?<span class="faq-icon">+</span></button>
+	        	<div class="faq-answer">?</div>
+	        </div>
+			<div class="faq-item">
+				<button class="faq-question">Как проходит обучение?<span class="faq-icon">+</span></button>
+				<div class="faq-answer">?</div>
+	        </div>
+	        <div class="faq-item">
+				<button class="faq-question">Чему вы научитесь?<span class="faq-icon">+</span></button>
+	        	<div class="faq-answer">?</div>
+	        </div>
+	        <div class="faq-item">
+	        	<button class="faq-question">Кем вы сможете работать?<span class="faq-icon">+</span></button>
+	        	<div class="faq-answer">?</div>
+	        </div>
+	        <div class="faq-item">
+	        	<button class="faq-question">Что вы получите по итогам обучения?<span class="faq-icon">+</span></button>
+	        	<div class="faq-answer">?</div>
+	        </div>
+    	</div>
+    </section>
+
+    <section class="supervisor">
+    	<div class="container">
+    		<h3>Академические руководители</h3>
+    		<div block__supervisor>
+    			<div class="first__supervisor">
+    				<h3>Академические руководители</h3>
+    			</div>
+    			<div class="second__supervisor">
+    				<h3>Академические руководители</h3>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+    <!-- knopka-faq  -->
+    <script>
+      document.querySelectorAll('.faq-question').forEach(button => {
+      button.addEventListener('click', () => {
+      const currentlyActive = document.querySelector('.faq-question.active');
+      const answer = button.nextElementSibling;
+        if (currentlyActive && currentlyActive !== button) {
+          currentlyActive.classList.remove('active');
+          currentlyActive.nextElementSibling.style.display = 'none';
+        }
+        const isActive = button.classList.toggle('active');
+        answer.style.display = isActive ? 'block' : 'none';
+      });
+    });
+    </script>
+
+</body>
+</html>
